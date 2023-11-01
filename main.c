61b4ff2364883cabb88b5e7580950c5e3730f81b
EricBernardes <ericbernardes70@gmail.com>
2023-11-01 18:12:13 -0300

    Identacao

--- main.c
+++ main.c
@@ -3,22 +3,21 @@
 #define max 5
 
 void selecao(int *v) {
-int menor;
+  int menor;
 
-for(int i=0; i<max-1; i++){
-    menor=i;
-    for(int j=i+1; j<max; j++){
-        if(v[j]<v[menor])
-            menor=j;
+  for (int i = 0; i < max - 1; i++) {
+    menor = i;
+    for (int j = i + 1; j < max; j++) {
+      if (v[j] < v[menor])
+        menor = j;
     }
-    if(i!=menor){
-        int aux= v[i];
-        v[i]= v[menor];
-        v[menor]=aux;
+    if (i != menor) {
+      int aux = v[i];
+      v[i] = v[menor];
+      v[menor] = aux;
     }
+  }
 }
-}
-
 
 void imprimeVetor(int *v) {
   for (int i = 0; i < max; i++) {
